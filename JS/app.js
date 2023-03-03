288a57004ef00a9929795d8e31724f6b4d3d1ed2
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-04 00:01:42 +0600

    see more btn done

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,17 @@
-// load data from api 
+// load data from api
 const loadData = async () => {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-}
-loadData()
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const seeMoreSection = document.getElementById("see-more-section");
+    if (data.data.tools.length > 6) {
+      seeMoreSection.classList.remove("d-none");
+      displayData(data.data.tools.slice(0, 6));
+    }
+  } catch (error) {
+    console.log("Error!!!" + error);
+  }
+};
+
+loadData();
