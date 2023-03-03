cfbbbd5010c29bef5e9e883875b845cbdd3f67b5
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-04 01:14:42 +0600

    see more btn done

--- JS/app.js
+++ JS/app.js
@@ -14,24 +14,39 @@ const loadData = async () => {
   }
 };
 const displayData = (data) => {
-    // console.log(data)
-    const divContainer = document.getElementById('divSection')
-    divContainer.innerHTML = ''
-    // getting every object 
-    data.forEach(element => {
-        // console.log(element.published_in)
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = 
-        `<div class="card p-3">
-        <img src="${element.image?element.image:'<i class="fa-sharp fa-solid fa-circle-exclamation"></i>'}" class="card-img-top" alt="...">
+  // console.log(data)
+  const divContainer = document.getElementById("divSection");
+  divContainer.innerHTML = "";
+  // getting every object
+  data.forEach((element) => {
+    // console.log(element.published_in)
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `<div class="card p-3">
+        <img src="${
+          element.image
+            ? element.image
+            : '<i class="fa-sharp fa-solid fa-circle-exclamation"></i>'
+        }" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Features</h5>
             <div>
                 <ol type="1" id="listItem">
-                    <li>${element.features[0] ? element.features[0] : '<b class="text-danger">No Data Found</b>'}</li>
-                    <li>${element.features[1] ? element.features[1] : '<b class="text-danger">No Data Found</b>'}</li>
-                    <li>${element.features[2] ? element.features[2] : '<b class="text-danger">No Data Found</b>'}</li>
+                    <li>${
+                      element.features[0]
+                        ? element.features[0]
+                        : '<b class="text-danger">No Data Found</b>'
+                    }</li>
+                    <li>${
+                      element.features[1]
+                        ? element.features[1]
+                        : '<b class="text-danger">No Data Found</b>'
+                    }</li>
+                    <li>${
+                      element.features[2]
+                        ? element.features[2]
+                        : '<b class="text-danger">No Data Found</b>'
+                    }</li>
                 </ol>
             </div>
             <h5>${element.name}</h5>
@@ -46,14 +61,30 @@ const displayData = (data) => {
     
                 </div>
                 <div>
-                    <button onclick="modalDetails('${element.id}')" class="arrow-btn" data-bs-toggle="modal"
+                    <button onclick="modalDetails('${
+                      element.id
+                    }')" class="arrow-btn" data-bs-toggle="modal"
                         data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right-long"></i></button>
                 </div>
             </div>
-        </div>`
-        
-        divContainer.appendChild(div)
-    });
-}
-loadData()
+        </div>`;
 
+    divContainer.appendChild(div);
+  });
+};
+
+// see more btn working function
+document.getElementById("see-more-btn").addEventListener("click", function () {
+  const seeMoreBtn = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      displayData(data.data.tools);
+    } catch (error) {
+      console.log("Error!!!!;" + error);
+    }
+  };
+  seeMoreBtn();
+});
+loadData();
